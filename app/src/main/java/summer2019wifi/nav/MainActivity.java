e08d5ee803be7fb0ca2db62c13ebf62fea64b13f
Kathryn Ng <ng.kathryn@gmail.com>
2019-08-01 16:31:32 -0700

    Update

--- app/src/main/java/summer2019wifi/nav/MainActivity.java
+++ app/src/main/java/summer2019wifi/nav/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WifiManager wManager;
 
-    private WifiReceiver wRec = new WifiReceiver();
+    //private WifiReceiver wRec = new WifiReceiver();
     private String SSID, BSSID;
     private int RSSI;
 
@@ -42,31 +43,33 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
             Log.d("WinFo", info.toString());
-            if (ConnectivityManager.TYPE_WIFI == info.getType()){
-                if (info != null && info.isConnected()){
-
-
+            if (ConnectivityManager.TYPE_WIFI == info.getType()) {
+                if (info != null && info.isConnected()) {
 
                     WifiInfo winfo = wManager.getConnectionInfo();
                     SSID = winfo.getSSID();
                     BSSID = winfo.getBSSID();
                     RSSI = winfo.getRssi();
 
-                   list = wManager.getScanResults();
-                   size = list.size();
-
                 }
             }
         }
-
-//        public List<ScanResult> getList(){
-//            return list;
-//        }
-//
-//        public int getSize(){
-//            return size;
-//        }
     };
+
+    private BroadcastReceiver receiverScanner = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                list = wManager.getScanResults();
+                unregisterReceiver(this);
+                Log.d("WinFo", String.valueOf(list.size()));
+                size = list.size();
+
+
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,42 +89,90 @@ public class MainActivity extends AppCompatActivity {
 
 
         wManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        final IntentFilter intentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        final IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        registerReceiver(receiver, intentFilter);
 
-        Button buttonScan = findViewById(R.id.buttonScan);
-        buttonScan.setOnClickListener(new View.OnClickListener(){
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                arraylist.clear();
+            public void run() {
+                registerReceiver(receiver, intentFilter); //displaying connected AP
+
+                registerReceiver(receiverScanner, new IntentFilter((WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))); // display list
+
                 wManager.startScan();
+                arraylist.clear();
+
+                labelNetworks.setText(SSID + " " + BSSID + " " + RSSI);
 
                 Toast.makeText(MainActivity.this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
                 try
                 {
-                    size = size - 1;
-                    while (size >= 0)
-                    {
-                        Log.d("Item",list.get(size).BSSID);
-                        HashMap<String, String> item = new HashMap<String, String>();
-                        item.put(ITEM_KEY, list.get(size).SSID + "  " + list.get(size).capabilities);
+                    int counter = 0;
 
-                        arraylist.add(item);
-                        size--;
+                    while (counter < size) {
+                        Log.d("Winfo", list.get(counter).SSID + " " + list.get(counter).BSSID + list.get(counter).level);
+
+                        HashMap<String, String> item = new HashMap<String, String>();
+                        if (list.get(counter).SSID.equals("ubcsecure")) {
+                            item.put(ITEM_KEY, list.get(counter).SSID + "  " + list.get(counter).BSSID + " " + list.get(counter).level);
+                            arraylist.add(item);
+                        }
+                        counter++;
                         simpleAdapter.notifyDataSetChanged();
                     }
                 }
                 catch (Exception e)
-                { }
+                { Log.d("WinFo", e.toString());};
 
-                // String networkName =
 
-                labelNetworks.setText(SSID + " " + BSSID + " " + RSSI);
-
+                handler.postDelayed(this, 5000);
             }
-        });
+        }, 5000);
+
+//        Button buttonScan = findViewById(R.id.buttonScan);
+//        buttonScan.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                arraylist.clear();
+//
+//                labelNetworks.setText(SSID + " " + BSSID + " " + RSSI);
+//
+//                Toast.makeText(MainActivity.this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+//                try
+//                {
+//                    size = size - 1;
+//
+//                    while (size >= 0) {
+//                        Log.d("Winfo", list.get(size).SSID + " " + list.get(size).BSSID);
+//
+//                        HashMap<String, String> item = new HashMap<String, String>();
+//                        if (list.get(size).SSID.equals("ubcsecure")) {
+//                            item.put(ITEM_KEY, list.get(size).SSID + "  " + list.get(size).BSSID + " " + list.get(size).level);
+//                            arraylist.add(item);
+//                        }
+//                        size--;
+//                        simpleAdapter.notifyDataSetChanged();
+//                    }
+//                }
+//                catch (Exception e)
+//                { Log.d("WinFo", e.toString());};
+//
+//            }
+//        });
+
     }
 
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
+    }
+
+
 }
+
+
 
--- app/src/main/java/summer2019wifi/nav/WifiReceiver.java
+++ app/src/main/java/summer2019wifi/nav/WifiReceiver.java
@@ -1,11 +1,15 @@
+// https://stackoverflow.com/questions/21391395/get-ssid-when-wifi-is-connected
+
 package summer2019wifi.nav;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 public class WifiReceiver extends BroadcastReceiver {
     protected String BSSID = "None";
@@ -13,13 +17,27 @@ public class WifiReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-        if (info != null && info.isConnected()){
-            WifiManager wManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
-            WifiInfo winfo = wManager.getConnectionInfo();
-            setBSSID(winfo.getBSSID());
-            setNetworkName(winfo.getSSID());
-        }
+
+              Log.d("START", "checking for connection");
+
+           if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())){
+               NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+               if (ConnectivityManager.TYPE_WIFI == info.getType()){
+                   if (info != null && info.isConnected()){
+                       Log.d("CONNECT", "info is connected");
+                       WifiManager wManager = (WifiManager) context.getApplicationContext().getSystemService(context.WIFI_SERVICE);
+                       WifiInfo winfo = wManager.getConnectionInfo();
+                       setBSSID(winfo.getBSSID());
+                       setNetworkName(winfo.getSSID());
+               }else{
+                       Log.d("Fail2", "Wifi not same");
+                   }
+           }else{
+                   Log.d("Fail", "Connection failed");
+               }
+        }else{
+               Log.d("Fail3", "connection failed");
+           }
     }
 
     public void setNetworkName(String networkName){
